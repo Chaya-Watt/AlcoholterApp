df0c8df4ea249747f2bc20806c0980ac09fd1415
Napatsorn-nut <Napatsorn1998pim@gmail.com>
2021-03-13 15:58:07 +0700

    add ButtonAlcohol

---
+++ Component/ButtonAlcohol.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import { Modal, StyleSheet, Text, Pressable, View, Image } from "react-native";
+
+const ButtonAlcohol = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View style={{backgroundColor:'black',flex:1,padding:50}}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {setModalVisible(!modalVisible);}}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>เมาแค่ไหน?</Text>
+            <Text style={styles.modalText1}>mg%     อาการที่ร่างกายแสดงออก</Text>
+            <Text style={styles.modalText2}>30                   ร่าเริงกว่าปกติ</Text>
+            <Text style={styles.modalText2}>50            ควบคุมการเคลื่อนไหว</Text>
+            <Text style={{fontSize:20,marginLeft:140}}>ได้ไม่ปกติ</Text>
+            <Text style={styles.modalText2}>100                เดินไม่ตรงทาง</Text>
+            <Text style={styles.modalText2}>200            สับสน พูดไม่รู้เรื่อง</Text>
+            <Text style={styles.modalText2}>300                  ง่วง อาเจียน</Text>
+            <Text style={styles.modalText2}>400             สลบและถึงตายได้</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>ปิด</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        onPress={() => setModalVisible(true)}
+      >
+        <Image
+                source={require('../Icons/information.png')}
+                style={{
+                  width: 20,
+                  height: 20
+                }}
+              />
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: -20,
+  
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    width:80,
+    height:40,
+    marginTop:5,
+    marginLeft:100
+  },
+  buttonClose: {
+    backgroundColor: "red",
+  },
+  textStyle: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  modalText: {
+    fontSize: 22,
+    marginBottom: 5,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  modalText1: {
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  modalText2: {
+    fontSize:20,
+    marginLeft:5
+  }
+});
+
+export default ButtonAlcohol;
---
+++ Component/ButtonInformation.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Modal, StyleSheet, Text, Pressable, View, Image } from "react-native";
+
+const ButtonInformation = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {setModalVisible(!modalVisible);}}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>คำนวณเวลา</Text>
+            <Text style={styles.modalText1}>วิธีการคำนวณหาระดับแอลกอฮอล์ในลมหายใจ เป็นเพียงค่าประมาณเท่านั้น ระดับแอลกอฮอล์ในลมหายใจของแต่ละคนมีความแตกต่างกัน ขึ้นอยู่กับหลายปัจจัย ยกตัวอย่างเช่น อัตราการเผาผลาญ ชนิดของเครื่องดื่ม ระยะเวลาในการดื่ม อาหารที่รับประทานก่อนดื่ม เป็นต้น</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>ปิด</Text>
+              <Text style={styles.textStyle}>ปิด</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        onPress={() => setModalVisible(true)}
+      >
+        <Image
+                source={require('../Icons/information.png')}
+                style={{
+                  width: 20,
+                  height: 20
+                }}
+              />
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+    
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 15,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    width:80,
+    height:40,
+    alignItems: "center",
+    marginTop:5
+  },
+  buttonClose: {
+    backgroundColor: "red",
+  },
+  textStyle: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  modalText: {
+    fontSize: 20,
+    marginBottom: 5,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  modalText1: {
+    fontSize: 17,
+    textAlign: "center",
+  }
+});
+
+export default ButtonImformation;
