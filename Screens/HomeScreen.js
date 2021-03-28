55d9430d6f76aa41ddc06351ebf6757f97a72c50
Chaya Watt <genechayanan@gmail.com>
2021-03-28 16:42:50 +0700

    uncomment bluetooth

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -64,83 +64,83 @@ const HomeScreen = ({navigation}) => {
     navigation.addListener('focus',()=> setLoading(!loading))
   },[navigation,loading])
 
-  // useEffect(() => { 
-  //   //Use useEffect for do onstateChange function
-  //   const subscription = manager.onStateChange((state) => {
-  //     manager.enable();
-  //     if (state === 'PoweredOn') {
-  //       console.log(state);
-  //       scanAndConnect();
-  //       subscription.remove();
-  //     }
-  //   }, true);
-  //   return () => {
-  //     console.log('Out');
-  //     clearInterval(TimerReadData);
-  //     setDevice("Device Don't Connect");
-  //     manager.cancelDeviceConnection('24:0A:C4:59:39:CE');
-  //     manager.disable();
-  //   };
-  // }, []);
+  useEffect(() => { 
+    //Use useEffect for do onstateChange function
+    const subscription = manager.onStateChange((state) => {
+      manager.enable();
+      if (state === 'PoweredOn') {
+        console.log(state);
+        scanAndConnect();
+        subscription.remove();
+      }
+    }, true);
+    return () => {
+      console.log('Out');
+      clearInterval(TimerReadData);
+      setDevice("Device Don't Connect");
+      manager.cancelDeviceConnection('24:0A:C4:59:39:CE');
+      manager.disable();
+    };
+  }, []);
 
-  // const scanAndConnect = async () =>
-  //   //Scan Device
-  //   {
-  //     console.log('Scan...Device');
-  //     setDevice('Scan Device');
-  //     manager.startDeviceScan(null, null, (error, device) => {
-  //       if (error) {
-  //         console.log(error);
-  //         return;
-  //       }
-  //       if (device.name === 'ESP32 BLE') {
-  //         console.log('Found', device.name);
-  //         manager.stopDeviceScan();
-  //         console.log('Stop Scan');
-  //         setDevice(device.name);
-  //         device
-  //           .connect()
-  //           .then((deviceDis) => {
-  //             //Discover device all service and characteristics
-  //             console.log('Discover All Services And Characteristics');
-  //             return deviceDis.discoverAllServicesAndCharacteristics();
-  //           })
-  //           .then((device) => {
-  //             //Have DeviceID and Send to function ReadInfoID
-  //             console.log('DeviceID : ' + device.id);
-  //             return ReadInfoID(device);
-  //           })
-  //           .catch((error) => {
-  //             console.log(error);
-  //           });
-  //       }
-  //     });
-  //   };
+  const scanAndConnect = async () =>
+    //Scan Device
+    {
+      console.log('Scan...Device');
+      setDevice('Scan Device');
+      manager.startDeviceScan(null, null, (error, device) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        if (device.name === 'ESP32 BLE') {
+          console.log('Found', device.name);
+          manager.stopDeviceScan();
+          console.log('Stop Scan');
+          setDevice(device.name);
+          device
+            .connect()
+            .then((deviceDis) => {
+              //Discover device all service and characteristics
+              console.log('Discover All Services And Characteristics');
+              return deviceDis.discoverAllServicesAndCharacteristics();
+            })
+            .then((device) => {
+              //Have DeviceID and Send to function ReadInfoID
+              console.log('DeviceID : ' + device.id);
+              return ReadInfoID(device);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      });
+    };
 
-  // //Read Service and Characteristic for device
-  // const ReadInfoID = async (device) => {
-  //   const ServicesID = await device.services();
-  //   console.log('ServiceID : ' + ServicesID[2].uuid); //Check ServiceUUID
+  //Read Service and Characteristic for device
+  const ReadInfoID = async (device) => {
+    const ServicesID = await device.services();
+    console.log('ServiceID : ' + ServicesID[2].uuid); //Check ServiceUUID
 
-  //   const CharacteristicID = await device.characteristicsForService(
-  //     ServicesID[2].uuid,
-  //   );
-  //   console.log('CharacteristicID : ' + CharacteristicID[0].uuid); //Check CharacteristicID
-  //   TimerReadData(device, ServicesID[2].uuid, CharacteristicID[0].uuid);
-  // };
+    const CharacteristicID = await device.characteristicsForService(
+      ServicesID[2].uuid,
+    );
+    console.log('CharacteristicID : ' + CharacteristicID[0].uuid); //Check CharacteristicID
+    TimerReadData(device, ServicesID[2].uuid, CharacteristicID[0].uuid);
+  };
 
-  // //Read Data for device
-  // const TimerReadData = async (device, Service, Characteristic) => {
-  //   setInterval(async () => {
-  //     const Data = await device.readCharacteristicForService(
-  //       Service,
-  //       Characteristic,
-  //     );
-  //     const realData = Math.floor(base64.decode(Data.value));
-  //     console.log('Value : ' + realData);
-  //     setData(realData);
-  //   }, 5000);
-  // };
+  //Read Data for device
+  const TimerReadData = async (device, Service, Characteristic) => {
+    setInterval(async () => {
+      const Data = await device.readCharacteristicForService(
+        Service,
+        Characteristic,
+      );
+      const realData = Math.floor(base64.decode(Data.value));
+      console.log('Value : ' + realData);
+      setData(realData);
+    }, 5000);
+  };
 
   return (
     <SafeAreaView>
