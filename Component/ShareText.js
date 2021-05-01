357113e21215068d3007aa1ba2b368f22a80be5b
Chaya Watt <genechayanan@gmail.com>
2021-05-01 20:18:48 +0700

    Edit sharetext

--- Component/ShareText.js
+++ Component/ShareText.js
@@ -2,13 +2,13 @@ import {Share} from 'react-native'
 import Symptom from '../Component/CalculateSymptom'
 
 
-const ShareText = async (Data) => {
+const ShareText = async (Data,name) => {
         try {
           console.log('Data: ',Data)
           const text =  Symptom({Data})
           console.log('text: ',text)
           const result = await Share.share({
-            message: Data + ' ' + text,
+            message: 'คุณ '+ name +' มีระดับปริมาณแอลกอฮอล์: '+Data + ' mg% ' + ' มีอาการ: '+text,
           });
           if (result.action === Share.sharedAction) {
             if (result.activityType) {
